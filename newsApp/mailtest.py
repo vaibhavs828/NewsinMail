3dacb81333e19443d908611ed85f4acf7d8f9ec3
vaibhavs828 <49330907+vaibhavs828@users.noreply.github.com>
2020-12-24 21:10:42 +0530

    testing with less news(critical worker timeout)

--- newsApp/mailtest.py
+++ newsApp/mailtest.py
@@ -38,55 +38,55 @@ def main():
         ind += 1
 
 
-    print("NOW printing world")
-    driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
-    driver.implicitly_wait(wait_time)
-    element2 = driver.find_elements_by_tag_name('h3')
+    #print("NOW printing world")
+    #driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
+    #driver.implicitly_wait(wait_time)
+    #element2 = driver.find_elements_by_tag_name('h3')
 
 
     #Saving into string
-    wnews = ''
-    ind2 = 1
-    for j in element2:
-        wnews += str(ind2) + ')'
-        wnews += j.text + '\n'
-        ind2 += 1
-        if ind2 == 7 :
-            break
+    #wnews = ''
+    #ind2 = 1
+    #for j in element2:
+     #   wnews += str(ind2) + ')'
+      #  wnews += j.text + '\n'
+       # ind2 += 1
+        #if ind2 == 7 :
+         #   break
         
-    print(wnews)
-    print("\n ")
-    print("NOW printing business")
-    driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
-    driver.implicitly_wait(wait_time)
-    element3 = driver.find_elements_by_tag_name('h3')
+   # print(wnews)
+    #print("\n ")
+    #print("NOW printing business")
+    #driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
+    #driver.implicitly_wait(wait_time)
+    #element3 = driver.find_elements_by_tag_name('h3')
 
     #Saving into string
-    bnews = ''
-    ind3 = 1
-    for k in element3:
-        bnews += str(ind3) + ')'
-        bnews += k.text + '\n'
-        ind3 += 1
-        if ind3 == 7 :
-            break
+   # bnews = ''
+    #ind3 = 1
+    #for k in element3:
+     #   bnews += str(ind3) + ')'
+      #  bnews += k.text + '\n'
+       # ind3 += 1
+        #if ind3 == 7 :
+         #   break
 
 
-    print("\n ")
-    print("NOW printing sports")
-    driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
-    driver.implicitly_wait(wait_time)
-    element4 = driver.find_elements_by_tag_name('h3')
+   # print("\n ")
+    #print("NOW printing sports")
+    #driver.get('https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&   gl=IN&ceid=IN%3Aen')
+    #driver.implicitly_wait(wait_time)
+    #element4 = driver.find_elements_by_tag_name('h3')
 
     #Saving into string
-    snews = ''
-    ind4 = 1
-    for l in element4:
-        snews += str(ind4) + ')'
-        snews += l.text + '\n'
-        ind4 += 1
-        if ind4 == 7 :
-            break
+    #snews = ''
+    #ind4 = 1
+    #for l in element4:
+     #   snews += str(ind4) + ')'
+      #  snews += l.text + '\n'
+       # ind4 += 1
+        #if ind4 == 7 :
+        #    break
 
 
 
@@ -108,15 +108,6 @@ def main():
     <h3 style="color:SlateGray;">Today's top news headlines</h3>
     <pre>
     """+news+"""</pre>
-    <h3 style="color:SlateGray;">World news headlines</h3>
-    <pre>
-    """+wnews+"""</pre>
-    <h3 style="color:SlateGray;">Business news headlines</h3>
-    <pre>
-    """+bnews+"""</pre>
-    <h3 style="color:SlateGray;">Sports news headlines</h3>
-    <pre>
-    """+snews+"""</pre>
     </body>
     </html>
     """#, subtype='html')
