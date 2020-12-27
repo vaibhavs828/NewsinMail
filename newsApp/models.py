97e5c0c6d8dc01d3c996777b37f3979865ba2433
vaibhavs828 <49330907+vaibhavs828@users.noreply.github.com>
2020-12-27 23:01:07 +0530

    Update models.py

--- newsApp/models.py
+++ newsApp/models.py
@@ -5,8 +5,12 @@ class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     email = models.CharField(max_length=50) 
+    topnews = models.BooleanField(null=True)
+    wnews = models.BooleanField(null=True)
+    bnews = models.BooleanField(null=True)
+    snews = models.BooleanField(null=True)
     #Type = models.CharField(max_length=)
-    frequency = models.CharField(max_length=10)
+    #frequency = models.CharField(max_length=10)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
